ffc5e3de707c82914dc19f003b64d4adb29b7e54
Srusti Thakkar <srustithakkar3295@gmail.com>
2018-01-04 14:36:28 +0530

    First commit

---
+++ WPFMVVMSample/Classes/User.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVMSample.Classes
+{
+    public class User : INotifyPropertyChanged
+    {
+        #region Private Properties
+        private int userId;
+        private string userName;
+        private string city;
+        private string state;
+        private string country;
+        #endregion
+
+        #region Public Properties
+        public int UserId
+        {
+            get
+            { return userId; }
+            set
+            {
+                userId = value;
+                OnPropertyChange("UserId");
+            }
+        }
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChange("UserName");
+            }
+        }
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+                OnPropertyChange("City");
+            }
+        }
+        public string Country
+        {
+            get
+            {
+                return country;
+            }
+            set
+            {
+                country = value;
+                OnPropertyChange("Country");
+            }
+        }
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+                OnPropertyChange("State");
+            }
+        }
+        #endregion
+
+        #region INotifyProperty Changed Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ WPFMVVMSample/ViewModels/UserViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPFMVVMSample.Classes;
+
+namespace WPFMVVMSample.ViewModels
+{
+    class UserViewModel
+    {
+        private IList<User> _UsersList;
+
+        public UserViewModel()
+        {
+            _UsersList = new List<User>
+            {
+                new User{UserId = 1,UserName="Srusti Thakkar" ,City="Anand",State="GUJ",Country="INDIA"},
+
+            };
+        }
+
+        public IList<User> Users
+        {
+            get
+            {
+                return _UsersList;
+            }
+            set
+            {
+                _UsersList = value;
+            }
+        }
+
+        private ICommand Ucommand;
+
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (Ucommand == null)
+                    Ucommand = new Updater();
+                return Ucommand;
+            }
+            set
+            {
+                Ucommand = value;
+            }
+        }
+        private class Updater : ICommand
+        {
+            #region ICommand Members
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+
+            }
+
+            #endregion
+        }
+    }
+}
